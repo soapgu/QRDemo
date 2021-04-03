092ecb93d7bd9d4bc3fd797d8a4083bd057c7dbb
guhui <guhui@shgbit.com>
2021-04-03 22:57:00 +0800

    1 add registerForActivityResult for SCAN Intent 2 add registerForActivityResult for RequestPermission of CAMERA

--- app/src/main/java/com/soapdemo/qrdemo/MainActivity.java
+++ app/src/main/java/com/soapdemo/qrdemo/MainActivity.java
@@ -1,9 +1,13 @@
 package com.soapdemo.qrdemo;
 
-import androidx.annotation.Nullable;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -12,24 +16,37 @@ import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityResultLauncher<Intent> mGetQRCode = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                Intent data = result.getData();
+                int resultCode = result.getResultCode();
+                IntentResult scanResult = IntentIntegrator.parseActivityResult(IntentIntegrator.REQUEST_CODE, resultCode, data);
+                if (scanResult != null) {
+                    // handle scan result
+                    Toast.makeText( getApplicationContext() , scanResult.toString(), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+    ActivityResultLauncher<String> mRequestPermission = registerForActivityResult( new ActivityResultContracts.RequestPermission() ,
+            result -> {
+                Toast.makeText( getApplicationContext() , result ? "同意,请重试":"拒绝", Toast.LENGTH_SHORT).show();
+            });
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.findViewById(R.id.button).setOnClickListener( v -> {
-            IntentIntegrator integrator = new IntentIntegrator(this);
-            integrator.setSingleTargetApplication( this.getPackageName() );
-            integrator.initiateScan(IntentIntegrator.QR_CODE_TYPES,-1);
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                IntentIntegrator integrator = new IntentIntegrator(this);
+                integrator.setSingleTargetApplication(this.getPackageName());
+                mGetQRCode.launch(integrator.initiateScanIntent(IntentIntegrator.QR_CODE_TYPES, -1));
+            }
+            else {
+                mRequestPermission.launch( Manifest.permission.CAMERA );
+            }
         } );
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if (scanResult != null) {
-            // handle scan result
-            Toast.makeText(this, scanResult.toString(), Toast.LENGTH_SHORT).show();
-        }
     }
 }
--- zxing/src/main/java/com/google/zxing/integration/android/IntentIntegrator.java
+++ zxing/src/main/java/com/google/zxing/integration/android/IntentIntegrator.java
@@ -332,6 +332,46 @@ public class IntentIntegrator {
   }
 
   /**
+   * Just initiateScan and reture Scan Intent
+   * @param desiredBarcodeFormats names of {@code BarcodeFormat}s to scan for
+   * @param cameraId camera ID of the camera to use
+   * @return Scan Intent
+   */
+  public final Intent initiateScanIntent(Collection<String> desiredBarcodeFormats, int cameraId) {
+    Intent intentScan = new Intent(BS_PACKAGE + ".SCAN");
+    intentScan.addCategory(Intent.CATEGORY_DEFAULT);
+
+    // check which types of codes to scan for
+    if (desiredBarcodeFormats != null) {
+      // set the desired barcode types
+      StringBuilder joinedByComma = new StringBuilder();
+      for (String format : desiredBarcodeFormats) {
+        if (joinedByComma.length() > 0) {
+          joinedByComma.append(',');
+        }
+        joinedByComma.append(format);
+      }
+      intentScan.putExtra("SCAN_FORMATS", joinedByComma.toString());
+    }
+
+    // check requested camera ID
+    if (cameraId >= 0) {
+      intentScan.putExtra("SCAN_CAMERA_ID", cameraId);
+    }
+
+    String targetAppPackage = findTargetAppPackage(intentScan);
+    if (targetAppPackage == null) {
+       showDownloadDialog();
+    }
+    intentScan.setPackage(targetAppPackage);
+    intentScan.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    intentScan.addFlags(FLAG_NEW_DOC);
+    attachMoreExtras(intentScan);
+    //startActivityForResult(intentScan, REQUEST_CODE);
+    return intentScan;
+  }
+
+  /**
    * Start an activity. This method is defined to allow different methods of activity starting for
    * newer versions of Android and for compatibility library.
    *
